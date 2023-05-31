710d2b84b731e9edd67a3719a38970723ce60f3b
Zandile <zandileignz472@gmail.com>
2023-05-31 02:35:51 +0200

    Update footer.php
    added correct css path

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
     <title>Footer</title>
-    <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
