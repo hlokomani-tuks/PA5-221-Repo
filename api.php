7df815d3aab08dfbd3cd27951b6f48e58fbdce34
Sibisi Mathebe <sibisirobert12@gmail.com>
2023-06-03 17:12:54 +0200

    Implemented get_reviews in api.php

--- api.php
+++ api.php
@@ -4,31 +4,33 @@ session_start();
 
 header("Content-Type: application/json");
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
     $data = file_get_contents("php://input");
     $decoded = json_decode($data);
 
-    if($decoded->type == 'get_wines'){
-        $servername = "wheatley.cs.up.ac.za";
-        $password = "CEHUZ7KY54OP2QLWN5CWGXUG3MLIDW56";
-        $username = "u21488593";
-        $databasename = "u21488593_PA5";
-        
-        $conn = mysqli_connect($servername, $username, $password, $databasename);
-        
-        if($conn->connect_error){
-            die("Error connecting: " . $conn->connect_error);
-        }
+    $servername = "wheatley.cs.up.ac.za";
+    $password = "CEHUZ7KY54OP2QLWN5CWGXUG3MLIDW56";
+    $username = "u21488593";
+    $databasename = "u21488593_PA5";
+
+    $conn = mysqli_connect($servername, $username, $password, $databasename);
+
+    if ($conn->connect_error) {
+        header("HTTP/1.1 500 Internal Server Error");
+        die("Error connecting: " . $conn->connect_error);
+    }
+
+    if ($decoded->type == 'get_wines') {
 
         $sql = "SELECT Wine.*, WineType.grape_varieties, WineType.colour, WineType.body, WineType.sweetness, WineType.tannin, Winery.winery_id, Winery.name AS winery_name, Winery.country, Winery.province, Winery.farm, Winery.estate, Winery.rating, Winery.email, Winery.cellphone_number FROM Wine JOIN WineType ON Wine.type_id = WineType.type_id JOIN Winery ON Wine.winery_id = Winery.winery_id";
 
         $result = mysqli_query($conn, $sql);
 
-        if($result->num_rows > 0){
+        if ($result->num_rows > 0) {
             $wines = array();
 
-            while($row = $result->fetch_assoc()){
+            while ($row = $result->fetch_assoc()) {
                 $name = $row['name'];
                 $year = $row['year'];
                 $description = $row['description'];
@@ -43,7 +45,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 $country = $row['country'];
                 $province = $row['province'];
                 $farm = $row['farm'];
-                
+
 
                 $wine = array(
                     'name' => $name,
@@ -71,18 +73,47 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             );
 
             header('Content-Type: application/json');
-            echo json_encode($wines, JSON_PRETTY_PRINT);
+            echo json_encode($response, JSON_PRETTY_PRINT);
             die();
-
         }
         $conn->close();
-    }else if($decoded->type == 'get_review'){
-        
-    }
-    
-    
-    
-    else{
+    } else if ($decoded->type == 'get_reviews') {
+        $wine_id = $decoded->wine_id;
+        $result = $conn->query("
+            SELECT 
+                first_name, 
+                middle_initial,
+                last_name,
+                rating,
+                comment,
+                is_critic 
+            FROM Review 
+            JOIN User 
+            ON Review.user_id = User.user_id 
+            WHERE wine_id = '$wine_id'
+        ");
+
+        $reviews = [];
+
+        while ($row = $result->fetch_assoc()) {
+            $reviews[] = [
+                "first_name" => $row["first_name"],
+                "middle_initial" => $row["middle_initial"],
+                "last_name" => $row["last_name"],
+                "rating" => $row["rating"],
+                "comment" => $row["comment"],
+                "is_critic" => (bool) $row["is_critic"]
+            ];
+        }
+
+        $response = [
+            "status" => "success",
+            "timestamp" => time(),
+            "data" => $reviews
+        ];
+
+        echo json_encode($response);
+    } else {
         $response = array(
             'status' => 'error',
             'timestamp' => time(),
@@ -92,3 +123,4 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         die();
     }
 }
+?>
