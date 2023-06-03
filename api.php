b92fbc93fed01d07367d40bc5cdaee9b7e3e665f
Sibisi Mathebe <sibisirobert12@gmail.com>
2023-06-03 21:06:17 +0200

    Implemented getting a single wine with an ID

--- api.php
+++ api.php
@@ -23,13 +23,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($decoded->type == 'get_wines') {
 
+        if ($decoded->wine_id == null)
+            $decoded->wine_id = "%";
+
         if ($decoded->sort_by == null)
             $decoded->sort_by = "wine_id";
 
         if ($decoded->sort_in == null)
             $decoded->sort_in = "ASC";
 
-        // $sql = "SELECT Wine.*, WineType.grape_varieties, WineType.colour, WineType.body, WineType.sweetness, WineType.tannin, Winery.winery_id, Winery.name AS winery_name, Winery.country, Winery.province, Winery.farm, Winery.estate, Winery.rating, Winery.email, Winery.cellphone_number FROM Wine JOIN WineType ON Wine.type_id = WineType.type_id JOIN Winery ON Wine.winery_id = Winery.winery_id";
         $sql =
             "SELECT
                 wine_id,
@@ -39,86 +41,90 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 food_pairing,
                 image_url,
                 user_rating,
+                user_rating_count,
                 critic_rating,
+                critic_rating_count,
                 grape_varieties,
-                Winery.name AS winery_name
+                colour, 
+                body,
+                sweetness,
+                tannin,
+                country,
+                province, 
+                farm,
+                estate,
+                Winery.name AS winery_name,
+                email AS winery_email,
+                cellphone_number AS winery_cellphone
             FROM WineWithReview Wine
             JOIN WineType
             ON Wine.type_id = WineType.type_id
             JOIN Winery
             ON Wine.winery_id = Winery.winery_id
+            WHERE wine_id LIKE '$decoded->wine_id'
             ORDER BY $decoded->sort_by $decoded->sort_in";
 
         $result = mysqli_query($conn, $sql);
 
-        if ($result->num_rows > 0) {
-            $wines = array();
+        $wines = array();
 
-            while ($row = $result->fetch_assoc()) {
-                /* 
-                $wine_id = $row['wine_id'];
-                $name = $row['name'];
-                $year = $row['year'];
-                $description = $row['description'];
-                $foodpair = $row['food_pairing'];
-                $imageurl = $row['image_url'];
-                $grape_varieties = $row['grape_varieties'];
-                $colour = $row['colour'];
-                $body = $row['body'];
-                $sweetness = $row['sweetness'];
-                $tannin = $row['tannin']; 
-                $winery_name = $row['winery_name'];
-                $country = $row['country'];
-                $province = $row['province'];
-                $farm = $row['farm'];
+        while ($row = $result->fetch_assoc()) {
+            $wines[] = [
+                "wine_id" => $row["wine_id"],
+                "name" => $row["name"],
+                "year" => $row["year"],
+                "description" => $row["description"],
+                "food_pairing" => $row["food_pairing"],
+                "image_url" => $row["image_url"],
+                "user_rating" => $row["user_rating"],
+                "user_rating_count" => $row["user_rating_count"],
+                "critic_rating" => $row["critic_rating"],
+                "critic_rating_count" => $row["critic_rating_count"],
+                "grape_varieties" => $row["grape_varieties"],
+                "colour" => $row["colour"],
+                "body" => $row["body"],
+                "sweetness" => $row["sweetness"],
+                "tannin" => $row["tannin"],
+                "country" => $row["country"],
+                "province" => $row["province"],
+                "farm" => $row["farm"],
+                "estate" => $row["estate"],
+                "winery_name" => $row["winery_name"],
+                "winery_email" => $row["winery_email"],
+                "winery_cellphone" => $row["winery_cellphone"]
+            ];
+        }
 
+        $response = [];
 
-                $wine = array(
-                    'wine_id' => $wine_id,
-                    'name' => $name,
-                    'year' => $year,
-                    'description' => $description,
-                    'foodpair' => $foodpair,
-                    'image' => $imageurl,
-                    'grape variety' => $grape_varieties,
-                    'colour' => $colour,
-                    'body' => $body,
-                    'sweetness' => $sweetness,
-                    'tannin' => $tannin,
-                    'winery_name' => $winery_name,
-                    'country' => $country,
-                    'province' => $province,
-                    'farm' => $farm
-                ); 
-                */
-
-                $wines[] = [
-                    'wine_id' => $row["wine_id"],
-                    'name' => $row["name"],
-                    'year' => $row["year"],
-                    'image' => $row["image_url"],
-                    'user_rating' => $row["user_rating"],
-                    'critic_rating' => $row["critic_rating"],
-                    'grape_variety' => $row["grape_varieties"],
-                    'winery_name' => $row["winery_name"]
-                ];
-            }
-
+        if ($decoded->wine_id == "%") {
             $response = array(
                 'status' => 'success',
                 'timestamp' => time(),
                 'data' => $wines
             );
-
-            header('Content-Type: application/json');
-            echo json_encode($response, JSON_PRETTY_PRINT);
-            die();
+        } else if (count($wines) == 1) {
+            $response = array(
+                'status' => 'success',
+                'timestamp' => time(),
+                'data' => $wines[0]
+            );
+        } else {
+            $response = array(
+                'status' => 'error',
+                'timestamp' => time(),
+                'error' => "No wine with given ID found"
+            );
         }
+
+        header('Content-Type: application/json');
+        echo json_encode($response, JSON_PRETTY_PRINT);
+
         $conn->close();
     } else if ($decoded->type == 'get_reviews') {
         $wine_id = $decoded->wine_id;
-        $result = $conn->query("
-            SELECT 
+        $result = $conn->query(
+            "SELECT 
                 first_name, 
                 middle_initial,
                 last_name,
@@ -128,8 +134,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             FROM Review 
             JOIN User 
             ON Review.user_id = User.user_id 
-            WHERE wine_id = '$wine_id'
-        ");
+            WHERE wine_id = '$wine_id'"
+        );
 
         $reviews = [];
 
@@ -151,15 +157,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         ];
 
         echo json_encode($response);
+
         $conn->close();
     } else {
         $response = array(
             'status' => 'error',
             'timestamp' => time(),
-            'Error: ' => 'That endpoint does not exist.'
+            'error' => 'That endpoint does not exist.'
         );
+
         echo json_encode($response, JSON_PRETTY_PRINT);
-        die();
+
         $conn->close();
     }
 }
