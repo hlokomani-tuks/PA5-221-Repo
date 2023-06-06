f1500ab4f25319923427436b19bcc59f3b83f54f
Sibisi Mathebe <sibisirobert12@gmail.com>
2023-06-06 14:12:25 +0200

    Made header react to login state and added logging out

--- logout.php
+++ logout.php
@@ -1,13 +1,4 @@
 <?php
-    include 'config.php';
-
-    unset($_SESSION["Email"]);
-    unset($_SESSION["UserName"]);
-    $_SESSION['LoggedIN'] = false;
-
-    unset($_COOKIE["Email"]);
-    unset($_COOKIE["UserName"]);
-
-    header("Location: index.php");
-    die;
+    session_start();
+    session_destroy();
 ?>
